c35b11a4a3cae6ce43bf823f6b8cf622972c9128
tate8 <70344865+tate8@users.noreply.github.com>
2021-05-14 14:31:06 -0600

    Add files via upload

---
+++ src/PDF_to_speech/main.py
@@ -0,0 +1,14 @@
+import PyPDF2
+import pyttsx3
+  
+path = open('file.pdf', 'rb')
+  
+pdfReader = PyPDF2.PdfFileReader(path)
+  
+from_page = pdfReader.getPage(0)
+  
+text = from_page.extractText()
+  
+speak = pyttsx3.init()
+speak.say(text)
+speak.runAndWait()
